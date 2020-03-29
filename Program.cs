68f65612f00f6e596b9a0e368831fa552b1e5d1c
John Gleason <john@jtpk.io>
2020-03-29 15:11:39 -0500

    arg for font size

--- Program.cs
+++ Program.cs
@@ -2,13 +2,21 @@
 using System.Text;
 using System.Drawing.Text;
 using System.IO;
+using System.Linq;
 
 namespace irl_fontbook
 {
     class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
+			int fontSize = 24;
+
+			if(args.Length > 0 && int.TryParse(args[0], out int size))
+			{
+				fontSize = size;
+			}
+
             var sb = new StringBuilder();
             sb.AppendLine(@"
 			<html>
@@ -32,11 +40,11 @@ namespace irl_fontbook
             var installedFonts = new InstalledFontCollection();
 
             int count = 1;
-            foreach (var fontFamily in installedFonts.Families)
+            foreach (var fontFamily in installedFonts.Families.OrderBy(f => f.Name))
             {
                 sb.AppendLine($@"<tr><td>{count++}</td>
 									<td>{fontFamily.Name}</td>
-									<td><span style=""font-size: 24px; font-family: {fontFamily.Name};"">
+									<td><span style=""font-size: {fontSize}px; font-family: {fontFamily.Name};"">
 										THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
 										<br />
 										The quick brown fox jumps over the lazy dog
